e097958342459f1d9b3a040d75dc862bf02fcbcc
arh91 <alvaro_rh_991@hotmail.com>
2023-09-13 10:24:42 +0200

    La aplicación pide al usuario que introduzca una ciudad cualquiera y le devuelve los datos del tiempo actuales para la misma.

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import requests as requests
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def conexion():
 
+    tecla = "w"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    while tecla!="s" and tecla!="S":
+        # Pedimos el nombre de la ciudad por teclado
+        ciudad = input("Introduzca el nombre de una ciudad:")
+        print("\n")
 
+        # Creamos un diccionario con los parámetros de la URL
+        parametros = {"q": ciudad,
+                      "units": "metric",
+                      "APPID": "89b9fdb6f95944991fd97b6727d65c78"}
+
+        # Realizamos la petición, indicando la URL y los parámetros
+        respuesta = requests.get("http://api.openweathermap.org/data/2.5/weather", params=parametros)
+
+        # Si la respuesta devuelve el código de estado 200, todo ha ido bien
+        if respuesta.status_code == 200:
+            # La respuesta json se convierte en un diccionario
+            datos = respuesta.json()
+            # Obtenemos descripción del tiempo del diccionario y la traducimos al español
+            estadoTiempo = datos["weather"][0]["main"]
+
+            if estadoTiempo == "Clear":
+                imprimirDatos(datos, "Despejado")
+            elif estadoTiempo == "Clouds":
+                imprimirDatos(datos, "Nubes")
+            elif estadoTiempo == "Mist":
+                imprimirDatos(datos, "Niebla")
+            elif estadoTiempo == "Rain":
+                imprimirDatos(datos, "Lluvia")
+            elif estadoTiempo == "Thunderstorm":
+                imprimirDatos(datos, "Tormenta")
+            elif estadoTiempo == "Snow":
+                imprimirDatos(datos, "Nieve")
+            else:
+                imprimirDatos(datos, estadoTiempo)
+
+        else:
+            print("De esa ciudad no tengo datos.")
+
+        tecla=input("Pulse S si quiere salir del programa o cualquier otra tecla si desea continuar:\n")
+
+def imprimirDatos(datos, descripcion):
+    print("Estado del tiempo actual: ", descripcion)
+    print("La temperatura actual es:", datos["main"]["temp"], "ºC")
+    print("La sensación térmica es:", datos["main"]["feels_like"], "ºC")
+    print("La temperatura mínima es:", datos["main"]["temp_min"], "ºC")
+    print("La temperatura máxima es:", datos["main"]["temp_max"], "ºC")
+    print("La presión es:", datos["main"]["pressure"], "hPa")
+    print("La humedad es:", datos["main"]["humidity"], "% \n")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    conexion()
